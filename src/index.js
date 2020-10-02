06a6eb475855e4d55c84cdc8684c18dcf09513f8
Dmitry Murzin <d.a.murzin@gmail.com>
2020-10-03 00:06:57 +0300

    feat: add solution to the task

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
 
 // You should implement your task here.
 
+const { sort } = require("semver")
+
 module.exports = function towelSort (matrix) {
-  return [];
+  if (matrix === undefined || matrix.length === 0) return []
+  for (let index = 0; index < matrix.length; index++) {
+    if ((index & 1) === 0) matrix[index].sort((a,b) => {return a - b})
+    else matrix[index].sort((a,b) => {return b - a})
+  }
+  return matrix.reduce((acc,value) => {return acc.concat(value)})
 }
